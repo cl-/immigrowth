4ea9f5d79d40f3d5b9eeed42c82f2a11e2ebe1f5
Yaadhav Raaj <raaj@lark.com>
2014-06-26 00:12:52 -0700

    update ajax

--- frontend/JS/ajax.js
+++ frontend/JS/ajax.js
@@ -1,44 +1,45 @@
 function Ajax(){
 
-	/*
-	This class handles the Ajax calls
-	*/
+    /*
+    This class handles the Ajax calls
+    */
 
 
 
-	var self=this;
+    var self=this;
+    var leg_id;
 
     var MyRequestsCompleted = (function() {
-	    var numRequestToComplete,
-	        requestsCompleted,
-	        callBacks,
-	        singleCallBack;
-	    return function(options) {
-	        if (!options) options = {};
+        var numRequestToComplete,
+            requestsCompleted,
+            callBacks,
+            singleCallBack;
+        return function(options) {
+            if (!options) options = {};
 
-	        numRequestToComplete = options.numRequest || 0;
-	        requestsCompleted = options.requestsCompleted || 0;
-	        callBacks = [];
-	        var fireCallbacks = function () {
-	           	stopSpinner();
-	            for (var i = 0; i < callBacks.length; i++) callBacks[i]();
-	        };
-	        if (options.singleCallback) callBacks.push(options.singleCallback);
+            numRequestToComplete = options.numRequest || 0;
+            requestsCompleted = options.requestsCompleted || 0;
+            callBacks = [];
+            var fireCallbacks = function () {
+                stopSpinner();
+                for (var i = 0; i < callBacks.length; i++) callBacks[i]();
+            };
+            if (options.singleCallback) callBacks.push(options.singleCallback);
 
-	        this.addCallbackToQueue = function(isComplete, callback) {
-	            if (isComplete) requestsCompleted++;
-	            if (callback) callBacks.push(callback);
-	            if (requestsCompleted == numRequestToComplete) fireCallbacks();
-	        };
-	        this.requestComplete = function(isComplete) {
-	            if (isComplete) requestsCompleted++;
-	            if (requestsCompleted == numRequestToComplete) fireCallbacks();
-	        };
-	        this.setCallback = function(callback) {
-	            callBacks.push(callBack);
-	        };
-	    };
-	})();
+            this.addCallbackToQueue = function(isComplete, callback) {
+                if (isComplete) requestsCompleted++;
+                if (callback) callBacks.push(callback);
+                if (requestsCompleted == numRequestToComplete) fireCallbacks();
+            };
+            this.requestComplete = function(isComplete) {
+                if (isComplete) requestsCompleted++;
+                if (requestsCompleted == numRequestToComplete) fireCallbacks();
+            };
+            this.setCallback = function(callback) {
+                callBacks.push(callBack);
+            };
+        };
+    })();
 
     function drawChart() {
 
@@ -51,8 +52,8 @@ function Ajax(){
 
     //https://app.fwd.us/api/v1/legislators/search.json?zip=94086&district=&state=&party=&key=f23fe9074cf28014359
 
-	/* Get Conversation GET Call */
-	this.searchLegislator = function(zip,callback){
+    /* Get Conversation GET Call */
+    this.searchLegislator = function(zip,callback){
         $.get("https://app.fwd.us/api/v1/legislators/search.json", { "zip":zip, "key":fwdAPIkey , "district":"", "state":"", "party":""})
         .done(function(jsonData) {
             //jsonData = JSON.parse(data);
@@ -71,13 +72,12 @@ function Ajax(){
 
         })
         .fail(function() { console.log("updateTree Error"); })
-	}
+    }
 
     this.getProfilePic =function(id,size,callback){
         $.get("https://graph.facebook.com/"+id+"/picture", { "type":size })
         .done(function(data) {
             //jsonData = JSON.parse(data);
-
             console.log(data);
             //callback(jsonData);
 
@@ -89,41 +89,109 @@ function Ajax(){
     }
 
     this.getB64Img =function(url,callback){
-        $.get(url, { })
+        // $.get(url, { })
+        // .done(function(data) {
+        //     //jsonData = JSON.parse(data);
+        //     console.log("**");
+        //     avatar=data;
+        //     //callback(jsonData);
+
+        //     //then we call this
+        //     //http://api.zippopotam.us/us/ca/sunnyvale
+
+        // })
+        // .fail(function() { console.log("getB64Img Error"); })
+
+
+
+                $.ajax({
+            dataType: 'text',
+            type: 'get',
+            url: url,
+            data: {
+                },
+            crossDomain: true,
+
+        })
         .done(function(data) {
             //jsonData = JSON.parse(data);
-            console.log("**");
-            img_data=data;
+            console.log("IMAGE SUCCESS");
+            avatar='data:image/png;base64,' + data;
+
+        })
+        .fail(function() { console.log("IMAGE Error"); })
+
+    }
+
+    this.sendFWD =function(callback){
+
+        //state make caps
+
+//         selfie[name]:Raaj
+// selfie[email]:raaj@raaj.com
+// selfie[zip]:94086
+// selfie[street_address]:geeg
+// selfie[city]:Sunnyvale
+// selfie[state]:CA
+// selfie[legislator_id]:H001034
+// selfie[message]:ergfwefwefwefwefwefwefwefewfwf wefwefwefwefwefwef efwegergergerg
+// selfie[avatar]:
+
+
+
+        $.ajax({
+            dataType: 'json',
+            type: 'post',
+            url: "https://app.fwd.us/api/v1/selfies.json",
+            data: {
+                "key":fwdAPIkey,
+                "selfie[name]":name,
+                "selfie[email]":email,
+                "selfie[zip]":zipcode,
+                "selfie[street_address]":address,
+                "selfie[city]":city,
+                "selfie[state]":stateDict[state].toUpperCase(),
+                "selfie[legislator_id]":leg_id,
+                "selfie[avatar]":avatar,
+                "selfie[message]":$('.Message').val()},
+            crossDomain: true,
+
+        })
+        .done(function(data) {
+            //jsonData = JSON.parse(data);
+            console.log("SELFIE SUCCESS");
+            console.log(data);
             //callback(jsonData);
 
             //then we call this
             //http://api.zippopotam.us/us/ca/sunnyvale
 
         })
-        .fail(function() { console.log("getB64Img Error"); })
-    }
+        .fail(function() { console.log("sendFWD Error"); })
 
-    this.sendFWD =function(callback){
 
-        state=stateDict[state];
-        state=state.toUpperCase();
+        $.ajax({
+            dataType: 'json',
+            type: 'post',
+            url: "https://app.fwd.us/api/v1/letters.json",
+            data: {
+                "key":fwdAPIkey,
+                "letter[name]":name,
+                "letter[email]":email,
+                "letter[zip]":zipcode,
+                "letter[street_address]":address,
+                "letter[city]":city,
+                "letter[state]":stateDict[state].toUpperCase(),
+                "letter[legislator_id]":leg_id,
+                "letter[shareable]":false,
+                "letter[writing]":$('.Writing').val()},
 
-        console.log("SENDING::");
-        console.log(fwdAPIkey);
-        console.log(name);
-        console.log(email);
-        console.log(zipcode);
-        console.log(address);
+            crossDomain: true,
 
-        console.log(city);
-        console.log(state);
-        console.log(leg_id);
-        console.log(message);
-        console.log(img_data);
-
-        $.post("http://staging.fwd.us/api/v1/selfies.json", { "key":fwdAPIkey, "name":name, "email":email , "zip":zipcode, "street_address":address, "city":city, "state":state, "legislator_id":leg_id, "avatar":img_data, "message":message,})
+        })
         .done(function(data) {
             //jsonData = JSON.parse(data);
+            console.log("LETTERS SUCCESS");
             console.log(data);
             //callback(jsonData);
 
